2d9bec1d3f23d43e5555664d054a12febe193890
Jonathan D'mello <jonathandmello@yahoo.co.in>
2021-12-07 18:38:13 +0530

    feat : working with Flask

--- public/station_goa_script.js
+++ public/station_goa_script.js
@@ -38,6 +38,92 @@ var chart1 = new Chart(ctx, {
   responsive: true
 });
 
+let  post_obj = {};
+const btn = document.getElementById("submit");
+btn.addEventListener("click", () => {
+  //post_obj.bpm = document.getElementById("bpm").value ;
+  post_obj.age = document.getElementById("age").value;
+  const sex = document.getElementById("sex").value;
+  if(sex == "Male")
+    post_obj.sex  = 1;
+  else
+    post_obj.sex = 0;
+  post_obj.resting_blood_pressure = document.getElementById("resting_blood_pressure").value;
+  post_obj.cholesterol = document.getElementById("cholesterol").value;
+  fasting_blood_sugar = document.getElementById("fasting_blood_sugar").value;
+  if(fasting_blood_sugar == "yes")
+    post_obj.fasting_blood_sugar = 1;
+  else
+    post_obj.fasting_blood_sugar = 0;
+  post_obj.max_heart_rate_achieved = document.getElementById("max_heart_rate_achieved").value;
+  exercise_induced_angina = document.getElementById("exercise_induced_angina").value;
+  if(exercise_induced_angina == "yes")
+    post_obj.exercise_induced_angina = 1;
+  else
+    post_obj.exercise_induced_angina = 0;
+  post_obj.st_depression = document.getElementById("st_depression").value;
+  post_obj.num_major_vessels = document.getElementById("num_major_vessels").value;
+  chestpain = document.getElementById("chest_pain_type").value;
+  post_obj.chest_pain_type_atypical_angina = 0;
+  post_obj.chest_pain_type_non_anginal_pain = 0;
+  post_obj.chest_pain_type_typical_angina = 0;
+  if(chestpain == "atypical angina")
+    post_obj.chest_pain_type_atypical_angina = 1;
+  else if(chestpain == "non-anginal pain")
+    post_obj.chest_pain_type_non_anginal_pain = 1;
+  else if(chestpain == "typical angina")
+    post_obj.chest_pain_type_typical_angina = 1;
+  post_obj.rest_ecg_type_left_ventricular_hypertrophy = 0;
+  post_obj.rest_ecg_type_normal 	= 0;
+  rest_ecg_type = document.getElementById("rest_ecg_type").value
+  if(rest_ecg_type == "normal")
+    post_obj.rest_ecg_type_normal  = 1;
+  else 
+    post_obj.rest_ecg_type_left_ventricular_hypertrophy = 1;
+  st_slope = document.getElementById("st_slope_type");
+  post_obj.st_slope_type_flat = 0;
+  post_obj.st_slope_type_upsloping = 1;
+  if(st_slope == "flat")
+    post_obj.st_slope_type_flat = 1;
+  else
+    post_obj.st_slope_type_upsloping = 1;
+  thalassemia_type = document.getElementById("thalassemia_type").value;
+  post_obj.thalassemia_type_normal 	= 0;
+  post_obj.thalassemia_type_nothing 	= 0;
+  post_obj.thalassemia_type_reversable_defect = 0;
+  post_obj.thalassemia_type_fixed_defect = 0;
+  if(thalassemia_type == "nothing")
+    post_obj.thalassemia_type_nothing 	= 1;
+  else if(thalassemia_type == "fixed defect")
+    post_obj.thalassemia_type_fixed_defect = 1;
+  else if(thalassemia_type == "normal")
+    post_obj.thalassemia_type_normal 	= 1;
+  else 
+    post_obj.thalassemia_type_reversable_defect = 1;
+  console.log(post_obj)
+  fetch("http://localhost:3002/uci", {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    method: "POST",
+    body: JSON.stringify(post_obj)
+})
+.then(function(res){ 
+  res.text().then( res => {
+    res = JSON.parse(res);
+    output = document.getElementById("prediction");
+    if(res.prediction == 1){
+      output.innerHTML = "High chance of heart disease";
+    }
+    else
+    output.innerHTML = "Low chance of heart disease";
+    console.log("PREDICTION",res) })
+  })
+  
+.catch(function(res){ console.log(res) })
+})
+
 let socket = io.connect("http://3.130.18.167:9000");
 
 socket.on("data", (data) => {
